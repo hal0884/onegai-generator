002f3e36af58801ea09cb7e83cf3f3127d67cb07
Takaharu <kendo3dan@gmail.com>
2011-11-24 22:58:10 +0900

    testdfs

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["user_info"]["name"]
-      user.screen_name = auth["user_info"]['image']
+      user.name = auth["info"]["name"]
+      user.screen_name = auth["info"]['image']
       user.screen_name = user.screen_name.gsub("square","large")
       puts user.screen_name
     end
